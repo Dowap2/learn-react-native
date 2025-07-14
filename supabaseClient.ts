2bd2e5661ea6eceeb14f45ff2bc417f670a7e0cd
Dowap2 <dowapdowari@gmail.com>
2025-07-14 16:05:40 +0900

    [remove] 불필요한 파일 제거

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -6,4 +6,5 @@ const extra = Constants.expoConfig?.extra || Constants.manifest?.extra;
 const SUPABASE_URL = extra.supabaseUrl;
 const SUPABASE_ANON_KEY = extra.supabaseAnonKey;
 
+console.log(SUPABASE_ANON_KEY, SUPABASE_URL);
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
